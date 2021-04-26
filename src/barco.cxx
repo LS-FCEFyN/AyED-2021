a4b895c534acba152bc08c27208f6edbc8810936
Lore | ロレッタ <eljefemaestro3087@hotmail.com>
2021-04-26 12:34:44 -0700

    Add files via upload

---
+++ src/barco.cxx
@@ -0,0 +1,33 @@
+/*
+ * barco.cxx
+ *
+ *  Created on: Apr 25, 2021
+ *      Author: Loretta
+ */
+
+/*
+ * Aqui encontraras las implementaciones de las declaraciones encontradas en
+ * "barco.hxx"
+ */
+
+#include<string>
+#include "./barco.hxx"
+
+Barco::Barco(std::string tipo, int posicion[4]) {
+	this->setTipo(tipo);
+	this->setPosicion(posicion);
+}
+
+void Barco::setPosicion(int posicion[4]) {
+	for(int i = 0; i < 4; i++) {
+		this->posicion[i] = posicion[i];
+	}
+}
+
+void Barco::setTipo(std::string tipo) {
+	this->tipo = tipo;
+}
+
+std::string Barco::getTipo() {
+	return this->tipo;
+}
---
+++ src/main.cxx
@@ -0,0 +1,23 @@
+/*
+ * main.cxx
+ *
+ *  Created on: Apr 25, 2021
+ *      Author: Loretta
+ */
+
+#include <iostream>
+#include <string>
+#include "./headers/stack_linked_list.hh"
+#include "./headers/queue_linked_list.hh"
+#include "./headers/barco.hxx"
+
+int main(int argc, char **argv) {
+
+	struct Node *xd = NULL;
+	    push(1,2,&xd);
+	    Enqueue(&xd);
+	    Print();
+	    Dequeue();
+	    Print();
+	return 0;
+}
