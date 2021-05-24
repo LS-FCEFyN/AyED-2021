0ce06471bc25eb7143e9ad8b79b4a250043cbabe
Lore | ロレッタ <eljefemaestro3087@hotmail.com>
2021-05-24 01:05:26 -0300

    Update Queue.hxx

--- Queue.hxx
+++ Queue.hxx
@@ -8,19 +8,30 @@
 #ifndef QUEUE_HXX_
 #define QUEUE_HXX_
 
-#include "./Stack.hxx"
-
-class Queue {
-private:
-	Stack *stack;
-	Queue *front;
-	Queue *rear;
-public:
-	Queue();
-	void Enqueue(Stack*);
-	void Dequeue();
-	Stack* Front();
-	bool isEmpty();
+template <class T>
+class QueueNode {
+	public:
+		T data;
+		QueueNode<T> *next;
+		QueueNode(T);
 };
+
+
+template <class T>
+class Queue {
+	QueueNode<T>* front;
+	QueueNode<T>* rear;
+
+	public:
+	Queue();
+	void Enqueue(T);
+	void Dequeue();
+	T Front();
+	bool isEmpty();
+
+};
+
+#include "./queue.cxx" /* Is it safe to do this ? */
+                       /* I should probably read the new ISO standar */
 
 #endif /* QUEUE_HXX_ */
