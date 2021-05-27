738c8f78a762f1fda3609b420ccef5c3d450c4d5
Lore | ロレッタ <eljefemaestro3087@hotmail.com>
2021-05-27 18:30:31 -0300

    Update narytree.cxx

--- narytree.cxx
+++ narytree.cxx
@@ -1,48 +1,30 @@
+#include "./queue.hxx"
 #include "./narytree.hxx"
-#include <iostream>
 
-
-template <typename T> nary_tree<T>::nary_tree() {
-  this->child = nullptr;
-  this->sibling = nullptr;
+template <typename T> nary_tree_node<T>::nary_tree_node(T data) {
+  sibling = child = nullptr;
+  this->data = data;
 }
 
+template <typename T> nary_tree<T>::nary_tree(T data) {
+    q = new Queue<nary_tree_node<T>*>();
+    parent = new nary_tree_node<T>(data);
+    q->Enqueue(parent);
+}
 
+template <typename T> void nary_tree<T>::addChild(T data) {
+        parent->child = new nary_tree_node<T>(data);
+        parent = parent->child;
+        q->Enqueue(parent);
+}
 
-template <typename T>
-nary_tree<T> *nary_tree<T>::addSibling(nary_tree<T> *parent) {
-  if (parent == nullptr) {
-    return nullptr;
-  } else {
-    while (parent->sibling) {
-      parent = parent->sibling;
+template <typename T> void nary_tree<T>::addSibling(T data) {
+
+}
+
+template <typename T> void nary_tree<T>::traverseTree() {
+    while(! q->isEmpty() ) {
+        std::cout << " " << q->Front()->data;
+        q->Dequeue();
     }
-    return (parent->sibling = new nary_tree());
-  }
-}
-
-template <typename T>
-nary_tree<T> *nary_tree<T>::addChild(nary_tree<T> *parent) {
-  if (parent == nullptr) {
-    return nullptr;
-  }
-  if (parent->child != nullptr) {
-    return addSibling(parent->child);
-  } else {
-    return (parent->child = new nary_tree());
-  }
-}
-
-template <typename T> void nary_tree<T>::traverseTree(nary_tree<T> *parent) {
-  if (parent == nullptr) {
-    return;
-  } else {
-    while (parent) {
-      std::cout << " " << parent->data;
-      if (parent->child) {
-        traverseTree(parent->child);
-      }
-      parent = parent->sibling;
-    }
-  }
 }
