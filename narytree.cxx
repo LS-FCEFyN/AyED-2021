52c1d9de515db96013ff6b0aca9b6ef07242c8c4
Lore | ロレッタ <eljefemaestro3087@hotmail.com>
2021-05-29 01:38:23 -0300

    Update narytree.cxx

--- narytree.cxx
+++ narytree.cxx
@@ -1,32 +1,55 @@
 #include "./queue.hxx"
 #include "./narytree.hxx"
-
 template <typename T> nary_tree_node<T>::nary_tree_node(T data) {
   sibling = child = nullptr;
   this->data = data;
 }
 
-template <typename T> nary_tree<T>::nary_tree(T data) {
+template <typename T> nary_tree<T>::nary_tree(int N) {
     q = new Queue<nary_tree_node<T>*>();
-    parent = new nary_tree_node<T>(data);
-    q->Enqueue(parent);
+    q1 = new Queue<nary_tree_node<T>*>();
+    n = N;
 }
 
 template <typename T> void nary_tree<T>::addChild(T data) {
         parent->child = new nary_tree_node<T>(data);
         parent = parent->child;
-        q->Enqueue(parent);
 }
 
 template <typename T> void nary_tree<T>::addSibling(T data) {
     parent->sibling = new nary_tree_node<T>(data);
     parent = parent->sibling;
-    q->Enqueue(parent);
 }
 
+template<typename T> void nary_tree<T>::add(T data)
+{
+    if(parent == nullptr) {
+        parent = new nary_tree_node<T>(data);
+        q1->Enqueue(parent);
+    }
+    else if(parent->child == nullptr && nodes == 0) {
+        addChild(data);
+        nodes++;
+        q->Enqueue(parent);
+        q1->Enqueue(parent);
+    }
+    else if(parent->sibling ==  nullptr && nodes < n) {
+        addSibling(data);
+        nodes++;
+        q->Enqueue(parent);
+        q1->Enqueue(parent);
+        if(nodes == n) {
+            nodes = 0;
+            parent = q->Front();
+            q->Dequeue();
+        }
+    }
+}
+
+
 template <typename T> void nary_tree<T>::traverseTree() {
-    while(! q->isEmpty() ) {
-        std::cout << " " << q->Front()->data;
-        q->Dequeue();
+    while(!q1->isEmpty()) {
+        std::cout << " " << q1->Front()->data;
+        q1->Dequeue();
     }
 }
